a3a172dcdd0554890c46a9ab8495713d5e6df8a2
azdeivids <deivids.egle@gmail.com>
2023-07-03 22:26:43 +0100

    Added app name to the environment.

--- environment.tf
+++ environment.tf
@@ -1,5 +1,5 @@
 resource azuredevops_environment main {
   count      = length(var.env_name)
   project_id = azuredevops_project.main.id
-  name       = var.env_name[count.index]
+  name       = "${var.app_name}-${var.env_name[count.index]}"
 }
