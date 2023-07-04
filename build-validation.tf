f8a1a73813d923252a44453c3ec37b5aad89410f
azdeivids <deivids.egle@gmail.com>
2023-07-04 18:20:23 +0100

    Setup build validation.

--- build-validation.tf
+++ build-validation.tf
@@ -8,9 +8,9 @@ resource azuredevops_branch_policy_build_validation main {
   blocking = true
 
   settings {
-    display_name        = "Example build validation policy"
+    display_name        = "${var.env_name[count.index]}-build-validation-policy"
     build_definition_id = azuredevops_build_definition.multi_stage[count.index].id
-    valid_duration      = 720
+    valid_duration      = 420
     filename_patterns = [
       "/terraform/*"
     ]
